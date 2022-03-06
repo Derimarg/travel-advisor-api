9e1c83e50e1c25308851c8146880d3cd236d82b8
Derimar Gray <derimargray@gmail.com>
2022-03-06 08:38:41 -0600

    render places details component

--- src/api/index.js
+++ src/api/index.js
@@ -3,28 +3,26 @@ import axios from "axios";
 const URL =
   "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
 
-const options = {
-  params: {
-    bl_latitude: "11.847676",
-    tr_latitude: "12.838442",
-    bl_longitude: "109.095887",
-    tr_longitude: "109.149359",
-  },
-  headers: {
-    "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-    "x-rapidapi-key": "b9eb99eb42mshfc276f1c479e93bp1d4bb9jsn018b3bb37322",
-  },
-};
-
-export const getPlacesData = async () => {
+export const getPlacesData = async (sw, ne) => {
   try {
     // request
     const {
       data: { data },
-    } = await axios.get(URL, options);
+    } = await axios.get(URL, {
+      params: {
+        bl_latitude: sw.lat,
+        tr_latitude: ne.lat,
+        bl_longitude: sw.lng,
+        tr_longitude: ne.lng,
+      },
+      headers: {
+        "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+        "x-rapidapi-key": "b9eb99eb42mshfc276f1c479e93bp1d4bb9jsn018b3bb37322",
+      },
+    });
+
     return data;
   } catch (error) {
-      console.log(error);
-      
+    console.log(error);
   }
 };
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,26 +4,27 @@ import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import Rating from "@material-ui/lab/Rating";
 
-// import mapStyles from "../../mapStyles";
 import useStyles from "./styles.js";
 
-const Map = () => {
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
   const matches = useMediaQuery("(min-width:600px)");
   const classes = useStyles();
-
+  
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
-        bootstrapURLKeys={{ key: 'AIzaSyCGaG4z8rPhKxATZM51NgnLytNlW3Ci6Y4' }}
-        defaultCenter={''}
-        center={''}
+        bootstrapURLKeys={{ key: "AIzaSyCGaG4z8rPhKxATZM51NgnLytNlW3Ci6Y4" }}
+        defaultCenter={coordinates}
+        center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
-        options={''}
-        onChange={''}
-        onChildClick={''}
-      >
-      </GoogleMapReact>
+        options={""}
+        onChange={e => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw})
+        }}
+        onChildClick={""}
+      ></GoogleMapReact>
     </div>
   );
 };
