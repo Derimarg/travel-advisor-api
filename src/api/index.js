eb35b690d6c56dd3364977676faa17f09c0f0071
Derimar Gray <derimargray@gmail.com>
2022-03-06 10:47:59 -0600

    add rating filter

--- src/api/index.js
+++ src/api/index.js
@@ -1,25 +1,25 @@
 import axios from "axios";
 
-const URL =
-  "https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary";
-
-export const getPlacesData = async (sw, ne) => {
+export const getPlacesData = async (type, sw, ne) => {
   try {
-    // request
     const {
       data: { data },
-    } = await axios.get(URL, {
-      params: {
-        bl_latitude: sw.lat,
-        tr_latitude: ne.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
-      },
-      headers: {
-        "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-        "x-rapidapi-key": "b9eb99eb42mshfc276f1c479e93bp1d4bb9jsn018b3bb37322",
-      },
-    });
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+          tr_latitude: ne.lat,
+        },
+        headers: {
+          "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
+          "x-rapidapi-key":
+            "b9eb99eb42mshfc276f1c479e93bp1d4bb9jsn018b3bb37322",
+        },
+      }
+    );
 
     return data;
   } catch (error) {
