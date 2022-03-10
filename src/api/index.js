6762edaeb4030c24583d89e0590d7cfc2ca9aee6
Derimar Gray <derimargray@gmail.com>
2022-03-09 19:03:20 -0600

    refactor

--- src/api/index.js
+++ src/api/index.js
@@ -14,8 +14,8 @@ export const getPlacesData = async (type, sw, ne) => {
           tr_latitude: ne.lat,
         },
         headers: {
+          "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
           "x-rapidapi-host": "travel-advisor.p.rapidapi.com",
-          "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
         },
       }
     );
@@ -28,20 +28,23 @@ export const getPlacesData = async (type, sw, ne) => {
 
 export const getWeatherData = async (lat, lng) => {
   try {
-    const { data } = await axios.get(
-      "https://community-open-weather-map.p.rapidapi.com/find",
-      {
-        params: {
-          lon: lng,
-          lat: lat,
-        },
-        headers: {
-          "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
-          "x-rapidapi-key": process.env.REACT_APP_RAPIDAPI_KEY,
-        },
-      }
-    );
-    return data;
+    if (lat && lng) {
+      const { data } = await axios.get(
+        "https://community-open-weather-map.p.rapidapi.com/find",
+        {
+          params: {
+            lon: lng,
+            lat: lat,
+          },
+          headers: {
+            "x-rapidapi-key": process.env.REACT_APP_RAPID_API_KEY,
+            "x-rapidapi-host": "community-open-weather-map.p.rapidapi.com",
+          },
+        }
+      );
+
+      return data;
+    }
   } catch (error) {
     console.log(error);
   }
--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -4,19 +4,19 @@ import { Paper, Typography, useMediaQuery } from "@material-ui/core";
 import LocationOnOutlinedIcon from "@material-ui/icons/LocationOnOutlined";
 import Rating from "@material-ui/lab/Rating";
 
-import mapStyles from "./mapStyles";
+import mapStyles from "../../mapStyles";
 import useStyles from "./styles.js";
 
 const Map = ({
   coords,
-  setCoordinates,
-  setBounds,
   places,
+  setCoords,
+  setBounds,
   setChildClicked,
   weatherData,
 }) => {
+  const matches = useMediaQuery("(min-width:600px)");
   const classes = useStyles();
-  const isDesktop = useMediaQuery("(min-width:600px)");
 
   return (
     <div className={classes.mapContainer}>
@@ -32,57 +32,60 @@ const Map = ({
           styles: mapStyles,
         }}
         onChange={e => {
-          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setCoords({ lat: e.center.lat, lng: e.center.lng });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
         onChildClick={child => setChildClicked(child)}
       >
-        {places?.map((place, i) => (
-          <div
-            className={classes.markerContainer}
-            lat={Number(place.latitude)}
-            lng={Number(place.longitude)}
-            key={i}
-          >
-            {!isDesktop ? (
-              <LocationOnOutlinedIcon color="primary" fontSize="large" />
-            ) : (
-              <Paper elevation={3} className={classes.paper}>
-                <Typography
-                  className={classes.typography}
-                  variant="subtitle2"
-                  gutterBottom
-                >
-                  {place.name}
-                </Typography>
-                <img
-                  className={classes.pointer}
-                  src={
-                    place.photo
-                      ? place.photo.images.large.url
-                      : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
-                  }
-                  alt={place.name}
-                />
-                <Rating
-                  name="read-only"
-                  size="small"
-                  value={Number(place.rating)}
-                  readOnly
-                />
-              </Paper>
-            )}
-          </div>
-        ))}
-        {weatherData?.list?.map((data, i) => (
-          <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
-            <img
-              height="70px"
-              src={`https://openweathermap.org/img/w/${data.weather[0].icon}.png`}
-              alt="weather icon"
-            />
-          </div>
-        ))}
+        {places.length &&
+          places.map((place, i) => (
+            <div
+              className={classes.markerContainer}
+              lat={Number(place.latitude)}
+              lng={Number(place.longitude)}
+              key={i}
+            >
+              {!matches ? (
+                <LocationOnOutlinedIcon color="primary" fontSize="large" />
+              ) : (
+                <Paper elevation={3} className={classes.paper}>
+                  <Typography
+                    className={classes.typography}
+                    variant="subtitle2"
+                    gutterBottom
+                  >
+                    {" "}
+                    {place.name}
+                  </Typography>
+                  <img
+                    className={classes.pointer}
+                    src={
+                      place.photo
+                        ? place.photo.images.large.url
+                        : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"
+                    }
+                    alt={place.name}
+                  />
+                  <Rating
+                    name="read-only"
+                    size="small"
+                    value={Number(place.rating)}
+                    readOnly
+                  />
+                </Paper>
+              )}
+            </div>
+          ))}
+        {weatherData?.list?.length &&
+          weatherData.list.map((data, i) => (
+            <div key={i} lat={data.coord.lat} lng={data.coord.lon}>
+              <img
+                src={`http://openweathermap.org/img/w/${data.weather[0].icon}.png`}
+                height="70px"
+                alt="weather icon "
+              />
+            </div>
+          ))}
       </GoogleMapReact>
     </div>
   );
